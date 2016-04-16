e5056d08f0d0c5d6415bf0cb69cde9a7fd2c7fe4
ZirconCode <gruenings@gmail.com>
2016-04-16 16:39:09 +0200

    morphing poly

--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Polygon;
+import java.awt.event.KeyEvent;
 
 
 public class Player extends Element{
@@ -18,7 +19,8 @@ public class Player extends Element{
 		p = new Polygon();
 		p.addPoint(0, 0);
 		p.addPoint(50, 0);
-		p.addPoint(100, 100);
+		p.addPoint(50, 50);
+		p.addPoint(0, 50);
 	}
 	
 	public void render(Graphics g)
@@ -29,11 +31,55 @@ public class Player extends Element{
 		
 		g.fillPolygon(p);
 		
+		g.setColor(Color.white);
+        g.drawString("V:"+area(p), (int)x, (int)y);
+		
 	}
 	
 	public void tick(GameState s)
 	{
-		p.xpoints[0] += 1;
+		p.translate(-(int)x, -(int)y);
+		// working from origin
+		
+		// idea: find nearest point and move it closer to mouse
+		
+		if(state.mouseDown)
+			p.xpoints[1] += 2;
+		else 
+			p.xpoints[1] = Math.max(Math.abs(p.xpoints[1] - 2), 50);
+				
+				
+		if(state.keyDown[KeyEvent.VK_S])
+			y += 1;
+		if(state.keyDown[KeyEvent.VK_W])
+			y += -1;
+		if(state.keyDown[KeyEvent.VK_D])
+			x += 1;
+		if(state.keyDown[KeyEvent.VK_A])
+			x += -1;
+
+		p.translate((int)x, (int)y);
+		// working beyond origin
+		
+		// TODO check bounds
+		
+
+			
+		//p.xpoints[0] += 1;
 	}
+	
+	// thanks math openref <3
+	public double area(Polygon poly)
+	{
+		double result = 0;
+		int j = poly.npoints-1;
+		for(int i=0; i<poly.npoints; i++)
+		{
+			result = result +  (poly.xpoints[j]+poly.xpoints[i]) * (poly.ypoints[j]-poly.ypoints[i]); 
+			j = i;  //j is previous vertex to i
+		}
+		return -1*result/2;
+	}
+
 	
 }
